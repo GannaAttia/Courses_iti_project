04a0345c5829e52eee1c1b632602989f7a85dec5
FatmaAyman2 <fatmaelkady2004@gmail.com>
2025-09-29 19:52:43 +0300

    profile1
    profile1

---
+++ app/Models/Student.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    protected $fillable = [
+        'name',
+        'phone',
+        'password',
+        'email',
+        'image',
+        'course_id',
+      
+    ];
+
+     public function course()
+    {
+        return $this->belongsTo(Course::class, 'course_id'); 
+        
+    }
+}
